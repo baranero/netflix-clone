613c6a525be1f4cef72893173a926977c63ff5d5
baranero <jakub.baran2@gmail.com>
2023-04-23 12:39:37 +0200

    made dynamic login form

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import Input from "@/components/Input";
 
 const Auth = () => {
@@ -6,6 +6,12 @@ const Auth = () => {
     const [email, setEmail] = useState('')
     const [name, setName] = useState('')
     const [password, setPassword] = useState('')
+
+    const [variant, setVariant] = useState('login')
+
+    const toggleVariant = useCallback(() => {
+        setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
+    }, [])
 
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-center bg-no-repeat">
@@ -16,15 +22,17 @@ const Auth = () => {
                 <div className="flex justify-center">
                     <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
                         <h2 className="text-white text-4xl mb-8 font-semibold">
-                            Sign in
+                            {variant === 'login' ? 'Sign in' : 'Register'}
                         </h2>
                         <div className="flex flex-col gap-4">
+                            {variant === 'register' && (
                         <Input
                                 label="Username"
                                 onChange={(event) => setEmail(event.target.value)}
                                 id="name"
                                 value={name}
                             />
+                            )}
                             <Input
                                 label="Email"
                                 onChange={(event) => setName(event.target.value)}
@@ -41,8 +49,14 @@ const Auth = () => {
                             />
                         </div>
                         <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-                            Login
+                            {variant === 'login' ? 'Login' : 'Register'}
                         </button>
+                        <p className="text-neutral-500 mt-12">
+                            {variant === 'login' ?' First time using Netflix?' : 'Already have an account?'}
+                            <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
+                            {variant === 'login' ?' Create an account' : 'Login'}
+                            </span>
+                        </p>
                     </div>
                 </div>
             </div>   
