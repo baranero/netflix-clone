9ee60eac860a9df323ecdff3b08732a9aac21fa1
baranero <jakub.baran2@gmail.com>
2023-04-25 23:05:46 +0200

    added user profile

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,13 +21,16 @@ export async function getServerSideProps(context: NextPageContext) {
 
 export default function Home() {
 
-
+  const { data: user } = useCurrentUser()
+  console.log(user);
+  
 
   return (
     <>
       <h1 className="text-2xl text-green-500">
         Netflix Clone
       </h1>
+      <p className="text-white">Logged in as : {user?.name}</p>
       <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout</button>
     </>
   )
