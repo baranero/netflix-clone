154f8af6995836354dc75e216bd479dd58ee311e
baranero <jakub.baran2@gmail.com>
2023-04-28 18:40:19 +0200

    added modal to movie card

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,10 +1,17 @@
 import useBillboard from "@/hooks/useBillboard"
 import { AiOutlineInfoCircle } from 'react-icons/ai'
 import PlayButton from "./PlayButton"
+import { useCallback } from "react"
+import useInfoModal from "@/hooks/useInfoModal"
 
 const Billboard = () => {
 
     const { data } = useBillboard()
+    const { openModal } = useInfoModal()
+
+    const handleOpenModal = useCallback(() => {
+        openModal(data?.id)
+    }, [openModal, data?.id])
     
     return (
         <div className="relative h-[56.25vw]">
@@ -49,7 +56,7 @@ const Billboard = () => {
                 </p>
                 <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
                     <PlayButton movieId={data?.id}/>
-                    <button className="
+                    <button onClick={handleOpenModal} className="
                         bg-white
                         text-white
                         bg-opacity-30
